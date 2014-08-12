7c3da6296e27668f454ff5c18d71216b51c389f8
David Winter <david.winter@gmail.com>
2014-08-12 15:07:50 -0700

    added bamtools back

---
+++ third-party/bamtools/build/src/toolkit/CMakeFiles/bamtools_cmd.dir/DependInfo.cmake
@@ -0,0 +1,45 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/david/src/bamtools/src/toolkit/bamtools.cpp" "/home/david/src/bamtools/build/src/toolkit/CMakeFiles/bamtools_cmd.dir/bamtools.cpp.o"
+  "/home/david/src/bamtools/src/toolkit/bamtools_convert.cpp" "/home/david/src/bamtools/build/src/toolkit/CMakeFiles/bamtools_cmd.dir/bamtools_convert.cpp.o"
+  "/home/david/src/bamtools/src/toolkit/bamtools_count.cpp" "/home/david/src/bamtools/build/src/toolkit/CMakeFiles/bamtools_cmd.dir/bamtools_count.cpp.o"
+  "/home/david/src/bamtools/src/toolkit/bamtools_coverage.cpp" "/home/david/src/bamtools/build/src/toolkit/CMakeFiles/bamtools_cmd.dir/bamtools_coverage.cpp.o"
+  "/home/david/src/bamtools/src/toolkit/bamtools_filter.cpp" "/home/david/src/bamtools/build/src/toolkit/CMakeFiles/bamtools_cmd.dir/bamtools_filter.cpp.o"
+  "/home/david/src/bamtools/src/toolkit/bamtools_header.cpp" "/home/david/src/bamtools/build/src/toolkit/CMakeFiles/bamtools_cmd.dir/bamtools_header.cpp.o"
+  "/home/david/src/bamtools/src/toolkit/bamtools_index.cpp" "/home/david/src/bamtools/build/src/toolkit/CMakeFiles/bamtools_cmd.dir/bamtools_index.cpp.o"
+  "/home/david/src/bamtools/src/toolkit/bamtools_merge.cpp" "/home/david/src/bamtools/build/src/toolkit/CMakeFiles/bamtools_cmd.dir/bamtools_merge.cpp.o"
+  "/home/david/src/bamtools/src/toolkit/bamtools_random.cpp" "/home/david/src/bamtools/build/src/toolkit/CMakeFiles/bamtools_cmd.dir/bamtools_random.cpp.o"
+  "/home/david/src/bamtools/src/toolkit/bamtools_resolve.cpp" "/home/david/src/bamtools/build/src/toolkit/CMakeFiles/bamtools_cmd.dir/bamtools_resolve.cpp.o"
+  "/home/david/src/bamtools/src/toolkit/bamtools_revert.cpp" "/home/david/src/bamtools/build/src/toolkit/CMakeFiles/bamtools_cmd.dir/bamtools_revert.cpp.o"
+  "/home/david/src/bamtools/src/toolkit/bamtools_sort.cpp" "/home/david/src/bamtools/build/src/toolkit/CMakeFiles/bamtools_cmd.dir/bamtools_sort.cpp.o"
+  "/home/david/src/bamtools/src/toolkit/bamtools_split.cpp" "/home/david/src/bamtools/build/src/toolkit/CMakeFiles/bamtools_cmd.dir/bamtools_split.cpp.o"
+  "/home/david/src/bamtools/src/toolkit/bamtools_stats.cpp" "/home/david/src/bamtools/build/src/toolkit/CMakeFiles/bamtools_cmd.dir/bamtools_stats.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "_FILE_OFFSET_BITS=64"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/david/src/bamtools/build/src/api/CMakeFiles/BamTools.dir/DependInfo.cmake"
+  "/home/david/src/bamtools/build/src/utils/CMakeFiles/BamTools-utils.dir/DependInfo.cmake"
+  "/home/david/src/bamtools/build/src/third_party/jsoncpp/CMakeFiles/jsoncpp.dir/DependInfo.cmake"
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "../src"
+  "../src/api"
+  "../src/utils"
+  "../src/third_party"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
