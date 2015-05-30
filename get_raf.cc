0f4f1df4585ce438288c37614a4e147182cc0fdb
David Winter <david.winter@gmail.com>
2015-05-30 14:19:56 -0700

    raf exe builds with cmake

--- get_raf.cc
+++ get_raf.cc
@@ -2,14 +2,41 @@
 #include <stdint.h>
 #include <string>
 #include <algorithm>
+#include <sys/stat.h>
+#include <unordered_map>
 
 #include "utils.h"
 #include "api/BamReader.h"
 #include "utils/bamtools_pileup_engine.h"
+#include "utils/bamtools_fasta.h"
 
 
 using namespace BamTools;
 using namespace std;
+
+typedef unordered_map<string, uint16_t> SampleMap;
+
+uint16_t base_index(char b){                                                                        
+    switch(b){                                                                                      
+        case 'A':                                                                                   
+        case 'a':                                                                                   
+            return 0;                                                                               
+         case 'T':                                                                                  
+         case 't':                                                                                  
+             return 3;                                                                              
+         case 'C':                                                                                  
+         case 'c':                                                                                  
+             return 1;                                                                              
+         case 'G':                                                                                  
+         case 'g':                                                                                  
+             return 2;                                                                              
+         case '-':                                                                                  
+         case 'N':                                                                                  
+             return -1 ;                                                                            
+         default: // Unknown base, alert in debug mode?                                             
+             return -1;                                                                             
+    }                                                                                               
+}  
 
 class RafVisitor: public PileupVisitor{
     public:
@@ -17,7 +44,7 @@ class RafVisitor: public PileupVisitor{
                     BamAlignment& ali, 
                     const Fasta& ref_genome,
                     const SamHeader& header,
-                    const SampleMap samples,
+                    const SampleMap& samples,
                     vector<uint32_t>& nref,
                     vector<uint32_t>& depth,
                     ostream *out_stream
@@ -39,7 +66,7 @@ class RafVisitor: public PileupVisitor{
         void Visit(const PileupPosition& pileupData){
             uint64_t pos  = pileupData.Position;                                                   
             m_ref_genome.GetBase(pileupData.RefId, pos, current_base);
-            uint16_t ref_base_idx = base_index(current_base)
+            uint16_t ref_base_idx = base_index(current_base);
             for(auto it = begin(pileupData.PileupAlignments);
                      it !=end(pileupData.PileupAlignments);
                      ++it){
@@ -49,62 +76,91 @@ class RafVisitor: public PileupVisitor{
                         string tag_id;
                         it->Alignment.GetTag("RG", tag_id);
                         uint32_t sindex = m_samples[tag_id];
-                        depth[sindex] += 1;
+                        m_depth[sindex] += 1;
                         if(b_index == ref_base_idx){
-                            nref[sindex] += 1;
+                            m_nref[sindex] += 1;
                         }
                     }
                 }
             }
             for (size_t i = 0; i< nsamp; i++){
-                *m_ostream << m_bam_ref[pileupData.RefId].RefName << '\t' 
-                           << pos << '\t' << i << '\t' << nref[i] << depth[i] 
-                           << endl;
+                if(m_depth[i] > 0){
+                    *m_ostream << m_bam_ref[pileupData.RefId].RefName << '\t' 
+                               << pos << '\t' << i << '\t' << m_nref[i] << m_depth[i] 
+                               << endl;
+                }
+
             }
-                              
+    }                          
     private:
         RefVector m_bam_ref;
-        vector<uint32_t>& m_nref;
-        vector<uint32_t>&m_depth;
+        Fasta m_ref_genome;
+        vector<uint32_t> m_nref;
+        vector<uint32_t>m_depth;
         ostream* m_ostream;
         int nsamp;
         SampleMap m_samples;
         SamHeader m_header;
         int m_ref_pos;
-        string m_initial_data;
         char current_base;
 };
         
 int main(int argc, char* argv[]){
     string bam_path = argv[1];
-    string out_prefix = argv[2];
+    string ref_file = argv[2];
+    string out_prefix = argv[3];
     ofstream result_stream (out_prefix + "_bases.tsv");
     
-
+    //Set up the Bam for parsing...
     BamReader bam;
     bam.Open(bam_path);
     bam.OpenIndex(bam_path + ".bai");
-    SamHeader header = bam.GetHeader();
-    SampleNames samples;
     RefVector references = bam.GetReferenceData(); 
+    SamHeader header = bam.GetHeader();
+
+    //Now the reference genome, creating index if there isn't already one
+    Fasta reference_genome;
+    struct stat file_info;
+    string faidx_path = ref_file + ".fai";
+    if (stat(faidx_path.c_str(), &file_info) !=0){
+        reference_genome.Open(ref_file);
+        reference_genome.CreateIndex(faidx_path);
+    }
+    reference_genome.Open(ref_file, faidx_path);
+
+    //Map readgroups to samples
+    SampleMap name_map; 
+    uint16_t sindex = 0;
     for(auto it = header.ReadGroups.Begin(); it!= header.ReadGroups.End(); it++){
         if(it->HasSample()){
-                samples.push_back(it->Sample);
+            auto s  = name_map.find(it->Sample);
+            if( s == name_map.end()){ // not in there yet
+                name_map[it->Sample] = sindex;
+                sindex += 1;
+            }
         }
     }
+    // And now, go back over the read groups to map RG:sample index
+    SampleMap samples;
+    for(auto it = header.ReadGroups.Begin(); it!= header.ReadGroups.End(); it++){
+        if(it->HasSample()){
+            samples[it->ID] = name_map[it->Sample];  
+        }
+    }
+
     uint32_t nsamp = samples.size();
-    vector<uint32_t> minor_denom (nsamp, 0);
-    vector<uint32_t> minor_count (nsamp, 0);
+    vector<uint32_t> nref (nsamp, 0);
+    vector<uint32_t> depth (nsamp, 0);
 
     BamAlignment ali;
     PileupEngine pileup;
 
-    FreqVisitor *f = new FreqVisitor(references, 
-                                    ali, 
+    RafVisitor *f = new RafVisitor(references, 
+                                    ali, reference_genome,
                                     header,
                                     samples,
-                                    minor_count,
-                                    minor_denom,
+                                    nref,
+                                    depth,
                                     &result_stream);
     pileup.AddVisitor(f); 
     while(bam.GetNextAlignment(ali)){
@@ -112,12 +168,6 @@ int main(int argc, char* argv[]){
     }
     pileup.Flush();
     
-    ofstream summary (out_prefix + "_summary.tsv");
-    for (size_t i=0; i < nsamp; i++){
-        summary << samples[i] << '\t'
-                << minor_count[i]/(double)minor_denom[i];
-    }
-
-
     return 0;
 }      
+
